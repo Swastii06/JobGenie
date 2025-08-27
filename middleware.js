baa67fef9ec78bbd977f4ebbbd4e084e5486c47c
Swastii06 <dashswastideepa@gmail.com>
2025-08-27 19:50:50 +0530

    edit

--- middleware.js
+++ middleware.js
@@ -16,7 +16,8 @@ export default clerkMiddleware(async (auth, req) => {
 
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
-    return redirectToSignIn();
+    const url = new URL(req.url);
+    return redirectToSignIn({ returnBackUrl: url.toString() });
   }
 
   return NextResponse.next();
@@ -25,7 +26,7 @@ export default clerkMiddleware(async (auth, req) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
